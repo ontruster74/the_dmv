91a744fcff1ad12ab34444fc1f73cc590670b19c
ontruster74 <ontruster74@gmail.com>
2024-12-13 10:29:15 -0700

    add testing services methods

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,5 +1,5 @@
 class Facility
-  attr_reader :name, :address, :phone, :services, :collected_fees, :registered_vehicle
+  attr_reader :name, :address, :phone, :services, :collected_fees, :registered_vehicles
 
   def initialize(facility_details)
     @name = facility_details[:name]
@@ -14,20 +14,48 @@ class Facility
     @services << service
   end
 
-  def register_vehicle 
-    
+  def register_vehicle(vehicle)
+    @registered_vehicles << vehicle 
+    if vehicle.age >= 25
+      @collected_fees += 25
+    elsif vehicle.engine == :ev
+      @collected_fees += 200
+    else
+      @collected_fees += 100
+    end
   end
 
   def administer_written_test(registrant)
-    
+    if services.include?('Written Test')
+      if (registrant.permit == true && registrant.age >= 16)
+        registrant.license_data[:written] = true
+      end
+      return registrant.license_data[:written]
+    else
+      return false
+    end
   end
 
   def administer_road_test(registrant)
-    
+    if services.include?('Road Test')
+      if registrant.license_data[:written] = true
+        registrant.license_data[:license] = true
+      end
+      return registrant.license_data[:license]
+    else
+      return false
+    end
   end
 
   def renew_drivers_license(registrant)
-    
+    if services.include?('Renew Drivers License')
+      if registrant.license_data[:license] = true
+        registrant.license_data[:renewed] = true
+      end
+      return registrant.license_data[:renewed]
+    else
+      return false
+    end
   end
 
 end
