a3c9b25b8fd41f8abbb9f8d3379e82706acf01ec
ontruster74 <ontruster74@gmail.com>
2024-12-13 09:33:09 -0700

    add iteration 2 method frameworks

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,14 +1,33 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, :address, :phone, :services, :collected_fees, :registered_vehicle
 
   def initialize(facility_details)
     @name = facility_details[:name]
     @address = facility_details[:address]
     @phone = facility_details[:phone]
     @services = []
+    @registered_vehicles = []
+    @collected_fees = 0
   end
 
   def add_service(service)
     @services << service
   end
+
+  def register_vehicle 
+    
+  end
+
+  def administer_written_test(registrant)
+    
+  end
+
+  def administer_road_test(registrant)
+    
+  end
+
+  def renew_drivers_license(registrant)
+    
+  end
+
 end
