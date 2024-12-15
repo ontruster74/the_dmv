ddacd7905304a651a885313768aa3d1ed854cf5f
ontruster74 <ontruster74@gmail.com>
2024-12-14 22:29:05 -0700

    refactor #create_vehicles test

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -3,17 +3,30 @@ require 'rspec'
 
 describe VehicleFactory do
   describe '#create_vehicles' do
-    it 'can create vehicles from external data' do
+    it 'can create vehicles from external data (Washington)' do
       factory = VehicleFactory.new
       wa_ev_registrations = DmvDataService.new.wa_ev_registrations
-      new_vehicles = factory.create_vehicles(wa_ev_registrations)
-      expect(new_vehicles[0].make).to eq("BMW")
-      expect(new_vehicles[0].model).to eq("i3")
-      expect(new_vehicles[0].vin).to eq("WBY8P8C51K")
-      expect(new_vehicles[0].year).to eq("2019")
-      expect(new_vehicles[0].engine).to eq(:ev)
-      expect(new_vehicles[0].plate_type).to eq(nil)
-      expect(new_vehicles[0].registration_date).to eq(nil)
+      washington_vehicles = factory.create_vehicles(wa_ev_registrations)
+
+      vehicle_zero = washington_vehicles[0]
+      expect(vehicle_zero).to be_a(Vehicle)
+      expect(vehicle_zero.make).to eq("BMW")
+      expect(vehicle_zero.model).to eq("i3")
+      expect(vehicle_zero.vin).to eq("WBY8P8C51K")
+      expect(vehicle_zero.year).to eq("2019")
+      expect(vehicle_zero.engine).to eq(:ev)
+      expect(vehicle_zero.plate_type).to eq(nil)
+      expect(vehicle_zero.registration_date).to eq(nil)
+
+      vehicle_one = washington_vehicles[1]
+      expect(vehicle_one).to be_a(Vehicle)
+      expect(vehicle_one.make).to eq("FORD")
+      expect(vehicle_one.model).to eq("Mustang Mach-E")
+      expect(vehicle_one.vin).to eq("3FMTK4SE2P")
+      expect(vehicle_one.year).to eq("2023")
+      expect(vehicle_one.engine).to eq(:ev)
+      expect(vehicle_one.plate_type).to eq(nil)
+      expect(vehicle_one.registration_date).to eq(nil)
     end
   end
 end
