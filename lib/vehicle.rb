9e9685cfe462112347a3650b5d2ff3afd1536b36
ontruster74 <ontruster74@gmail.com>
2024-12-14 21:12:29 -0700

    add key parsing functionality to #initialize

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -5,17 +5,25 @@ class Vehicle
               :year,
               :make,
               :model,
-              :engine,
-              :registration_date,
-              :plate_type
+              :engine
+              
+  attr_accessor :plate_type, :registration_date
 
   def initialize(vehicle_details)
-    @vin = vehicle_details[:vin]
-    @year = vehicle_details[:year]
+    if vehicle_details.keys.include?(:vin_1_10)
+      @vin = vehicle_details[:vin_1_10]
+    else
+      @vin = vehicle_details[:vin]
+    end
+    if vehicle_details.keys.include?(:model_year)
+     @year = vehicle_details[:model_year]
+    else
+      @year = vehicle_details[:year]
+    end
     @make = vehicle_details[:make]
     @model = vehicle_details[:model]
-    @engine = vehicle_details[:engine]
-    @registration_date = vehicle_details[:registration_date]
+    @engine = vehicle_details.fetch(:engine, :ev)
+    @registration_date = nil
     @plate_type = nil
   end
 
